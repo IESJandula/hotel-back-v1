7ef4868ad0414eec64024cac373b6e33ef0a4757
Trufo218 <116663425+Trufo218@users.noreply.github.com>
2024-11-11 15:18:06 +0100

    Nuevas clases

--- src/main/java/es/iesjandula/hotelv1/gestionhotel/model/Pago.java
+++ src/main/java/es/iesjandula/hotelv1/gestionhotel/model/Pago.java
@@ -2,6 +2,7 @@
 package es.iesjandula.hotelv1.gestionhotel.model;
 
 //Importaciones.
+import es.iesjandula.hotelv1.gestionhotel.Enum.EstadoPago;
 import es.iesjandula.hotelv1.gestionhotel.Enum.MetodoPago;
 import jakarta.persistence.*;
 import java.util.Date;
